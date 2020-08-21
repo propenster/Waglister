aab1e594a0eaada72ae48795c7516a9b0ea3f73d
propenster <faitholusegun60@gmail.com>
2020-08-21 14:41:38 +0100

    Added Dashboard and Profile url and page

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
+    path('dashboard', views.user_dashboard, name='dashboard'),
+    path('profile', views.user_profile, name='profile'),
 ]
